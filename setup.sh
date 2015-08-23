a288ede27409873b12a980949daa30c96fabd56a
Vahid Fazel-Rezai <vahid@Vahids-MacBook-Pro.local>
2015-08-23 03:33:48 -0700

    fix git config typo

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
 ln -s $DIR"/.bash_profile" ~/.bash_profile;
-ln -s $DIR"/.giconfig" ~/.giconfig;
+ln -s $DIR"/.gitconfig" ~/.gitconfig;
 ln -s $DIR"/.gitexcludes" ~/.gitexcludes;
